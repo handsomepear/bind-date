1c146ab7854102893b12d4b7ea1da38a29e017cc
Zps <287879667@qq.com>
2020-05-17 21:37:53 +0800

    parseInt proxyId

--- src/utils/_toolkit.js
+++ src/utils/_toolkit.js
@@ -37,7 +37,7 @@ const toolkit = {
       sessionStorage.setItem('loginCount', ++loginCount)
       request({
         url: '/login',
-        data: { code: requestParams.code, proxyId: proxyId || '' }
+        data: { code: requestParams.code, proxyId: parseInt(proxyId) || '' }
       })
         .then(({ data }) => {
           success && success(data)
